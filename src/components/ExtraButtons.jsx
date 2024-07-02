6e145c7da8c372cf27d97f1600ab605572a898c8
mohit <mohitdhanawat001@gmail.com>
2024-07-02 10:32:20 +0530

    changes

--- src/components/ExtraButtons.jsx
+++ src/components/ExtraButtons.jsx
@@ -1,17 +1,9 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import Modal from 'react-native-modal';
-import { AppColors } from '../assets/Colors';
-import TopRightComponent from './MyBookingModal';
-import BookingsComponent from './Eeeeeeeeeeeeeeeeeeeeeeeeeee';
+import {AppColors} from '../assets/Colors';
 
 const ExtraButtons = ({OpenMyBookingModal}) => {
-  // const [myBookingModal, setMyBookingModal] = useState(false);
-  // const [myBookingModal, setMyBookingModal] = useState(true)
-
-  // const [myBookingPage, setMyBookingPage] = useState(false)
-
   const navigation = useNavigation();
   return (
     <View
@@ -49,15 +41,7 @@ const ExtraButtons = ({OpenMyBookingModal}) => {
           borderRadius: 5,
           backgroundColor: AppColors.mainColor,
         }}>
-        <TouchableOpacity
-        // onPress={() => navigation.navigate('MyComponent')}>
-        
-        onPress={() => OpenMyBookingModal(true)}
-        // onPress={() => setMyBookingModal(true)}OpenMyBookingModal
-        // onPress={() => setMyBookingPage(!myBookingPage)}
-        // onPress={() => console.warn('run')}
-        
-        >
+        <TouchableOpacity onPress={() => OpenMyBookingModal(true)}>
           <Text
             style={{
               padding: 7,
@@ -69,82 +53,7 @@ const ExtraButtons = ({OpenMyBookingModal}) => {
           </Text>
         </TouchableOpacity>
       </View>
-
-      {/* {myBookingModal ? <TopRightComponent/>  : null} */}
-
-
-      {/* {
-        myBookingPage ? <MyComponent setMyBookingPage={setMyBookingPage} /> : null
-      } */}
-      {/* <Modal
-        backdropOpacity={0}
-        onBackdropPress={() => setMyBookingModal(false)}
-        animationIn={'fadeInDown'}
-        animationOut={'fadeOutUp'}
-        isVisible={myBookingModal}>
-        <BookingsComponent setMyBookingModal={setMyBookingModal} />
-      </Modal> */}
     </View>
-    // ======================
-
-  //   <View
-  //   style={{
-  //     justifyContent: 'center',
-  //     alignItems: 'center',
-  //     display: 'flex',
-  //     flexDirection: 'row',
-  //     // marginTop:8
-  //   }}>
-  //   <View
-  //     style={{
-  //       margin: 5,
-  //       borderWidth: 1,
-  //       borderRadius: 5,
-  //       backgroundColor: '#16588e',
-  //     }}>
-  //     <TouchableOpacity onPress={() => navigate.navigate('TicketsDriver')}>
-  //       <Text
-  //         style={{
-  //           padding: 7,
-  //           fontSize: 10,
-  //           fontWeight: '500',
-  //           color: 'white',
-  //         }}>
-  //         Need Help ?
-  //       </Text>
-  //     </TouchableOpacity>
-  //   </View>
-  //   <View
-  //     style={{
-  //       margin: 5,
-  //       marginRight: 17,
-  //       borderWidth: 1,
-  //       borderRadius: 5,
-  //       backgroundColor: '#16588e',
-  //     }}>
-  //     <TouchableOpacity onPress={() => setMyBookingModal(true)}>
-  //       <Text
-  //         style={{
-  //           padding: 7,
-  //           fontSize: 10,
-  //           fontWeight: '500',
-  //           color: 'white',
-  //         }}>
-  //         My Bookings
-  //       </Text>
-  //     </TouchableOpacity>
-  //   </View>
-  // </View>
-  // <Modal
-  //   backdropOpacity={0}
-  //   //   backdropOpacity={.5}
-  //   onBackdropPress={() => setMyBookingModal(false)}
-  //   animationIn={'fadeInDown'}
-  //   animationOut={'fadeOutUp'}
-  //   isVisible={myBookingModal}>
-  //   <MyBookingModal setMyBookingModal={setMyBookingModal} />
-  // </Modal>
-
   );
 };
 

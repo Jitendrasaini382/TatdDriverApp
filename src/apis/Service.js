051edac0ecaff2939869537e807906e29380d1e6
mohit <mohitdhanawat001@gmail.com>
2024-07-18 22:19:39 +0530

    service update

--- src/apis/Service.js
+++ src/apis/Service.js
@@ -1,116 +1,4 @@
-// import axios from 'axios';
-// import {API_BASE_URL} from '../constant/path';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const getToken = async () => {
-//   const token = await AsyncStorage.getItem('jwt');
-//   return token;
-// };
-
-// const _Fetch = (method, path, body, header) => {
-//   return new Promise((resolve, reject) => {
-//     _handleMethod(method, path, body, header)
-//       .then(e => {
-//         console.log(e.status, 'APICALL SUCCESFUL');
-//         // if (e.status == 200) {
-//         if (e.data.status_code == 200) {
-//           resolve(e.data);
-//         } else {
-//           reject(e.data.message);
-//         }
-//       })
-//       .catch(err => {
-//         reject(err);
-//       });
-//   });
-// };
-
-// async function _handleMethod(method, path, body, header) {
-//   console.log(method, path, body, header, 'fetchHandle Data');
-
-//   if (method == 'GET') {
-//     return axios({
-//       method: 'GET',
-//       url: `${API_BASE_URL}${path}`,
-//       headers: {
-//         ...header,
-//         'Content-Type': 'text/plain',
-//       },
-//     });
-//   } else {
-//     return axios({
-//       method: method,
-//       url: `${API_BASE_URL}${path}`,
-//       data: body,
-//       headers: await changeHeaders(header),
-//     });
-//   }
-// }
-
-// async function changeHeaders(header) {
-//   const token = await getToken();
-//   // console.log(token, "ppppppppppppppppppppppppppppppppp");
-//   return {
-//     ...header,
-//     'Content-Type': 'text/plain',
-//     Authorization: `Bearer ${token}`,
-//   };
-// }
-
-// export default _Fetch;
-
-// //////////////////////////////////////////////
-
-// import axios from 'axios';
-// import {API_BASE_URL} from '../constant/path';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const getToken = async () => {
-//   return await AsyncStorage.getItem('jwt');
-// };
-
-// const _Fetch = async (method, path, body, header) => {
-//   try {
-//     const response = await _handleMethod(method, path, body, header);
-//     console.log(response.status, 'APICALL SUCCESSFUL');
-//     if (response.data.status_code == 200) {
-//       return response.data;
-//     } else {
-//       throw new Error(response.data.message);
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// async function _handleMethod(method, path, body, header) {
-//   console.log(method, path, body, header, 'fetchHandle Data');
-//   const url = `${API_BASE_URL}${path}`;
-//   const headers =
-//     method === 'GET'
-//       ? {...header, 'Content-Type': 'text/plain'}
-//       : await changeHeaders(header);
-
-//   return axios({
-//     method,
-//     url,
-//     data: method !== 'GET' ? body : undefined,
-//     headers,
-//   });
-// }
-
-// async function changeHeaders(header) {
-//   const token = await getToken();
-//   return {
-//     ...header,
-//     'Content-Type': 'text/plain',
-//     Authorization: `Bearer ${token}`,
-//   };
-// }
-
-// export default _Fetch;
-
-// ///////////////////////////////////////////////////////
+// ApiService.js
 
 import axios from 'axios';
 import {API_BASE_URL} from '../constant/path';
@@ -120,21 +8,39 @@ const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
+// Request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('jwt');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    config.headers['Content-Type'] = 'application/json';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
 api.interceptors.response.use(
   response => response,
   async error => {
     const originalRequest = error.config;
 
-    if (error.response.status === 400 && !originalRequest._retry) {
+    if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
         const refreshToken = await AsyncStorage.getItem('refresh_token');
 
-        // रिफ्रेश टोकन API कॉल (आपके बैकएंड के अनुसार URL और पैरामीटर्स अपडेट करें)
-        const response = await axios.get(`${API_BASE_URL}/login/refresh_token.php`, {
-          refresh_token: refreshToken
-        });
+        const response = await axios.get(
+          `${API_BASE_URL}/login/refresh_token.php`,
+          {
+            params: { refresh_token: refreshToken },
+          },
+        );
 
         const {jwt_token} = response.jwt;
 
@@ -145,7 +51,7 @@ api.interceptors.response.use(
 
         return api(originalRequest);
       } catch (refreshError) {
-        // रिफ्रेश टोकन भी इनवैलिड है, यहाँ लॉगआउट लॉजिक डालें
+        await AsyncStorage.multiRemove(['jwt', 'refresh_token']);
         return Promise.reject(refreshError);
       }
     }
@@ -154,15 +60,156 @@ api.interceptors.response.use(
   },
 );
 
-const getToken = async () => {
-  return await AsyncStorage.getItem('jwt');
+const _Fetch = (method, path, body, header = {}) => {
+  return new Promise((resolve, reject) => {
+    api({
+      method,
+      url: path,
+      data: method !== 'GET' ? body : undefined,
+      params: method === 'GET' ? body : undefined,
+      headers: header,
+    })
+      .then(response => {
+        console.log(response.status, 'APICALL SUCCESSFUL');
+        if (response.data.status_code == 200) {
+          resolve(response.data);
+        } else {
+          reject(response.data.message);
+        }
+      })
+      .catch(err => {
+        console.error('API CALL FAILED', err);
+        reject(err.response ? err.response.data : err.message);
+      });
+  });
 };
+
+export default _Fetch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import axios from 'axios';
+// // import {API_BASE_URL} from '../constant/path';
+// // import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// // const getToken = async () => {
+// //   return await AsyncStorage.getItem('jwt');
+// // };
+
+// // const _Fetch = async (method, path, body, header) => {
+// //   try {
+// //     const response = await _handleMethod(method, path, body, header);
+// //     console.log(response.status, 'APICALL SUCCESSFUL');
+// //     if (response.data.status_code == 200) {
+// //       return response.data;
+// //     } else {
+// //       throw new Error(response.data.message);
+// //     }
+// //   } catch (error) {
+// //     throw error;
+// //   }
+// // };
+
+// // async function _handleMethod(method, path, body, header) {
+// //   console.log(method, path, body, header, 'fetchHandle Data');
+// //   const url = `${API_BASE_URL}${path}`;
+// //   const headers =
+// //     method === 'GET'
+// //       ? {...header, 'Content-Type': 'text/plain'}
+// //       : await changeHeaders(header);
+
+// //   return axios({
+// //     method,
+// //     url,
+// //     data: method !== 'GET' ? body : undefined,
+// //     headers,
+// //   });
+// // }
+
+// // async function changeHeaders(header) {
+// //   const token = await getToken();
+// //   return {
+// //     ...header,
+// //     'Content-Type': 'text/plain',
+// //     Authorization: `Bearer ${token}`,
+// //   };
+// // }
+
+// // export default _Fetch;
+
+// // ///////////////////////////////////////////////////////
+
+// import axios from 'axios';
+// import {API_BASE_URL} from '../constant/path';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// const api = axios.create({
+//   baseURL: API_BASE_URL,
+// });
+
+// api.interceptors.response.use(
+//   response => response,
+//   async error => {
+//     const originalRequest = error.config;
+
+//     if (error.response.status == 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+
+//       try {
+//         const refreshToken = await AsyncStorage.getItem('refresh_token');
+
+//         const response = await axios.get(
+//           `${API_BASE_URL}/login/refresh_token.php`,
+//           {
+//             refresh_token: refreshToken,
+//           },
+//         );
+
+//         const {jwt_token} = response.jwt;
+
+//         await AsyncStorage.setItem('jwt', jwt_token);
+
+//         api.defaults.headers.common['Authorization'] = `Bearer ${jwt_token}`;
+//         originalRequest.headers['Authorization'] = `Bearer ${jwt_token}`;
+
+//         return api(originalRequest);
+//       } catch (refreshError) {
+//         return Promise.reject(refreshError);
+//       }
+//     }
+
+//     return Promise.reject(error);
+//   },
+// );
+
+// const getToken = async () => {
+//   return await AsyncStorage.getItem('jwt');
+// };
 
 // const _Fetch = (method, path, body, header) => {
 //   return new Promise((resolve, reject) => {
 //     _handleMethod(method, path, body, header)
 //       .then(e => {
-//         console.log(e.status, 'APICALL SUCCESFUL');
+//         console.log(e.status, 'APICALL SUCCESSFUL');
 //         if (e.data.status_code == 200) {
 //           resolve(e.data);
 //         } else {
@@ -175,61 +222,28 @@ const getToken = async () => {
 //   });
 // };
 
-// ///////////////
+// const _handleMethod = async (method, path, body, header) => {
+//   console.log(method, path, body, header, 'fetchHandle Data');
+//   const headers =
+//     method === 'GET'
+//       ? {...header, 'Content-Type': 'text/plain'}
+//       : await changeHeaders(header);
 
-// const _Fetch = async (method, path, body, header) => {
-//   try {
-//     const response = await _handleMethod(method, path, body, header);
-//     console.log(response.status, 'APICALL SUCCESSFUL');
-//     if (response.data.status_code == 200) {
-//       return response.data;
-//     } else {
-//       throw new Error(response.data.message);
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
+//   return api({
+//     method,
+//     url: path,
+//     data: method !== 'GET' ? body : undefined,
+//     headers,
+//   });
 // };
 
-const _Fetch = (method, path, body, header) => {
-  return new Promise((resolve, reject) => {
-    _handleMethod(method, path, body, header)
-      .then(e => {
-        console.log(e.status, 'APICALL SUCCESSFUL');
-        if (e.data.status_code == 200) {
-          resolve(e.data);
-        } else {
-          reject(e.data.message);
-        }
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-};
+// const changeHeaders = async header => {
+//   const token = await getToken();
+//   return {
+//     ...header,
+//     'Content-Type': 'text/plain',
+//     Authorization: `Bearer ${token}`,
+//   };
+// };
 
-const _handleMethod = async (method, path, body, header) => {
-  console.log(method, path, body, header, 'fetchHandle Data');
-  const headers =
-    method === 'GET'
-      ? {...header, 'Content-Type': 'text/plain'}
-      : await changeHeaders(header);
-
-  return api({
-    method,
-    url: path,
-    data: method !== 'GET' ? body : undefined,
-    headers,
-  });
-};
-
-const changeHeaders = async header => {
-  const token = await getToken();
-  return {
-    ...header,
-    'Content-Type': 'text/plain',
-    Authorization: `Bearer ${token}`,
-  };
-};
-
-export default _Fetch;
+// export default _Fetch;
